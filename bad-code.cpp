05f916da1d673aafb87ba27cab073fda52f91dea
Cheuk Lam Cheung <ccheung792423@gmail.com>
2020-10-15 18:38:45 -0400

    draft

--- bad-code.cpp
+++ bad-code.cpp
@@ -1,8 +1,17 @@
-                int main(){
+           #include <iostream>
+
+     #include <cstdlib>
+
+
+                                          int main(){
            // Hi, I'm a program!
-int x = 1; 
+   int x = 1;
+   
     for(int i = 0; i < 10; i++) {
-cout << i;
-          cout << endl;
- }
+      
+std::cout << i << std::endl;
+                    std::cout << (i%2) << std::endl;
+              std::cout << "End" << std::endl;
+          }
+        return 0;
     }
